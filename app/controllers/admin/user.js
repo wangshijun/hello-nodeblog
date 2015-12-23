667db0932e1a703be507372910b8d87ba8612f82
wangshijun2010 <wangshijun2010@gmail.com>
2015-12-24 07:23:58 +0800

    user register part 1

--- app/controllers/admin/user.js
+++ app/controllers/admin/user.js
@@ -4,9 +4,30 @@ var express = require('express'),
     Post = mongoose.model('Post');
 
 module.exports = function (app) {
-    app.use('/admin', router);
+    app.use('/admin/users', router);
 };
 
-router.get('/', function (req, res, next) {
-    res.redirect('/admin/posts')
+router.get('/login', function (req, res, next) {
+    res.render('admin/user/login', {
+    	pretty: true,
+    });
+});
+
+router.post('/login', function (req, res, next) {
+    res.jsonp(req.body);
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('admin/user/register', {
+    	pretty: true,
+    });
+});
+
+router.post('/register', function (req, res, next) {
+    res.jsonp(req.body);
+});
+
+router.get('/logout', function (req, res, next) {
+    // TODO
+    res.redirect('/');
 });
